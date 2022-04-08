0c4aa420efed6d0f545fe70f0c25cc043f8f59d4
hoanguyenphuoc <nphoa-batch9bd@sdc.edu.vn>
2022-04-08 15:52:37 +0700

    fix import

--- src/main/java/com/company/entity/Product.java
+++ src/main/java/com/company/entity/Product.java
@@ -5,10 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "watches",
+@Table(name = "products",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "slug")
         })
